66aa01bd787a76e06b7e2065d52b46ec22c026aa
MrseFuntik <136927535+MrseFuntik@users.noreply.github.com>
2024-01-14 15:21:44 +0300

    Update hse_bot.py
    Добавил команду /help, обновил команду /start

--- hse_bot.py
+++ hse_bot.py
@@ -14,6 +14,23 @@ bot = Bot(token=token)
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
 
+objects = '''
+⏺ small vehicle - маленький автомобиль
+⏺ large vehicle - большой автомобиль
+⏺ tennis court - теннисный корт
+⏺ ground track field - стадион
+⏺ ship - корабль
+⏺ harbor - гавань (порт)
+⏺ storage tank - большой резервуар
+⏺ swimming pool - бассейн
+⏺ plane - самолет
+⏺ bridge - мост
+⏺ roundabout - круговой перекрёсток
+⏺ baseball diamond - бейсбольное поле
+⏺ soccer ball field - футбольное поле
+⏺ basketball court - баскетбольная площадка
+⏺ helicopter - вертолёт
+'''
 
 class Form(StatesGroup):
     usless_state = State()  # на будущее
@@ -24,7 +41,18 @@ model = torch.hub.load('ultralytics/yolov5', 'custom', path='last.pt', force_rel
 
 @dp.message_handler(commands=["start"])  # command /start handler
 async def cmd_start(message: types.Message):
-    await message.answer('Отправь мне изображение и я верну тебе изображение с обозначенными объектами')
+    await message.answer('Приветствую. Наш бот детектит объекты, относящиеся к 15 различным классам, среди которых '
+                         'малый и крупный автотранспорт, самолеты, вертолеты, корабли и прочее. '
+                         'Отправь мне изображение (к примеру, это может быть снимок со спутника), и я верну тебе '
+                         'изображение с обозначенными объектами, если таковые найдутся')
+
+
+@dp.message_handler(commands=["help"])  # command /help handler
+async def cmd_help(message: types.Message):
+    await message.answer(f"Наш бот классифицирует следующие объекты:\n{objects}\n"
+                         "Чтобы получить изображение с классифицируемыми объектами, просто отправь боту изображение. "
+                         "Это может быть как сжатое изображение, так и исходный файл, главное чтобы расширение файла "
+                         "было либо .png, либо .jpg")
 
 
 @dp.message_handler(content_types=['photo', 'document'])  # от юзера принимает только сжатые фотки / исходники изображений
